6dfdf694e8594f4cb5d9c657ff3671dd29b6ecc7
abnerdouglas <abner.rmachado@gmail.com>
2025-03-30 20:39:11 -0300

    fix: corrige exibição da data

--- src/app/auth/home/page.tsx
+++ src/app/auth/home/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
                   <Table.Cell bg="white" color="black" textStyle="md">{expense.description}</Table.Cell>
                   <Table.Cell bg="white" color="black" textStyle="md">{expense.type}</Table.Cell>
                   <Table.Cell bg="white" color="black" textStyle="md">{formatCurrency(expense.value)}</Table.Cell>
-                  <Table.Cell bg="white" color="black" textStyle="md">{expense.date}</Table.Cell>
+                  <Table.Cell bg="white" color="black" textStyle="md"> {new Date(expense.date).toLocaleDateString("pt-BR")}</Table.Cell>
                   <Table.Cell bg="white" color="black" textStyle="md">{expense.user.name}</Table.Cell>
                   <Table.Cell bg="white" color="black" textStyle="md">
                     <ReadImage image={expense.image} />
